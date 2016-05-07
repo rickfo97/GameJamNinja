e75af9720e728a1fba71a0caecf8877423f8904b
Estrozix <simonnilsson1997@gmail.com>
2016-05-07 12:11:15 +0200

    no message

---
+++ ViveSandboxProj/Assets/_MyScripts/Damage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Target")
+        {
+            Destroy(other);
+            GameManager.gm.changeHealth(-10);
+        }
+    }
+}
---
+++ ViveSandboxProj/Assets/_MyScripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm;
+
+    private float health;
+
+    public Text healthText;
+
+	// Use this for initialization
+	void Start () {
+        health = 100;
+
+        updateHealthText();
+
+        if (gm == null)
+            gm = this.gameObject.GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeHealth(int modifier)
+    {
+        health += modifier;
+
+        updateHealthText();
+    }
+
+    void updateHealthText()
+    {
+        if(healthText != null)
+        {
+            healthText.text = "" + health;
+        }
+    }
+}
